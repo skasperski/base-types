844c4e9d116c6f9dc92e23e4ce896bad006a0879
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2022-03-25 11:28:40 +0100

    Removed SISL stuff.

--- bindings/ruby/ext/base_types_ruby/init.cpp
+++ bindings/ruby/ext/base_types_ruby/init.cpp
@@ -1,15 +1,8 @@
 #include "rice/Class.hpp"
 extern void Init_eigen_ext();
 
-#ifdef SISL_FOUND
-extern void Init_spline_ext();
-#endif
-
 extern "C" void Init_base_types_ruby()
 {
     Init_eigen_ext();
-#ifdef SISL_FOUND
-    Init_spline_ext();
-#endif
 }
 

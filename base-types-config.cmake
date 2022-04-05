04bc7583c0b7ac371ed596f6f70de5838f9b6948
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2022-04-05 15:20:40 +0200

    Fixed install locations.

--- base-types-config.cmake
+++ base-types-config.cmake
@@ -1,5 +1,5 @@
 include(CMakeFindDependencyMacro)
 find_dependency(Eigen3 REQUIRED)
 
-include("${CMAKE_CURRENT_LIST_DIR}/types-targets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/base-types-targets.cmake")
 
